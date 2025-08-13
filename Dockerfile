d440f8cb680dd12c5c2706bc6faa01761da8d861
Manuel Worschech <mw@abot.xyz>
2025-08-13 03:28:35 +0200

    Static file collection

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ FROM netboxcommunity/netbox:v4.3.5
 COPY ./plugin_requirements.txt /opt/netbox/
 RUN /usr/local/bin/uv pip install -r /opt/netbox/plugin_requirements.txt
 
+COPY ./configuration.py /etc/netbox/config/configuration.py
+COPY ./plugins.py /etc/netbox/config/plugins.py
 RUN DEBUG="true" SECRET_KEY="dummydummydummydummydummydummydummydummydummydummy" \
     /opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py collectstatic --no-input
---
+++ plugins.py
@@ -0,0 +1,26 @@
+PLUGINS = [
+    # 'ipfabric_netbox',
+    'netbox_acls',
+    'netbox_bgp',
+    'netbox_floorplan',
+    'netbox_dns',
+    'netbox_qrcode',
+    'netbox_reorder_rack',
+    'netbox_secrets',
+    'netbox_topology_views',
+    'netbox_diode_plugin',
+    # 'slurpit_netbox',
+    # 'nb_service', # Outdated as of 20250812
+    # 'nb_risk', # Outdated as of 20250812
+    'netbox_config_diff',
+    'netbox_contract',
+    'netbox_interface_synchronization',
+    'netbox_inventory',
+    'netbox_lifecycle',
+    'netbox_otp_plugin',
+    'netbox_prometheus_sd',
+    'netbox_routing',
+    'netbox_security',
+    'validity',
+    'netbox_branching',
+]
