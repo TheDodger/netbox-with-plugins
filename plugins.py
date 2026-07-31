ca0576497cd79f6a94d5bbfd5bf30aa18decf951
Manuel Worschech <mw@abot.xyz>
2026-07-31 22:14:28 +0200

    Update Netbox to 4.5.10 Add some plugins

--- plugins.py
+++ plugins.py
@@ -1,28 +1,35 @@
 PLUGINS = [
+    "inventory_monitor",
     # "ipfabric_netbox",
-    "netbox_acls",
-    "netbox_bgp",
-    "netbox_floorplan",
-    "netbox_dns",
-    "netbox_qrcode",
-    "netbox_reorder_rack",
-    "netbox_secrets",
-    "netbox_topology_views",
-    "netbox_diode_plugin",
-    # "slurpit_netbox",
-    # "nb_service", # Outdated as of 20250812
     # "nb_risk", # Outdated as of 20250812
+    # "nb_service", # Outdated as of 20250812
+    "netbox_acls",
+    "netbox_attachments",
+    "netbox_bgp",
+    "netbox_branching",
     "netbox_config_diff",
     "netbox_contract",
+    "netbox_custom_objects",
+    "netbox_data_flows",
+    # "netbox_dhcp", # Beta as of 20260726
+    "netbox_diode_plugin",
+    "netbox_dns",
+    "netbox_documents",
+    "netbox_floorplan",
     "netbox_interface_synchronization",
     "netbox_inventory",
+    "netbox_ipcalculator",
     "netbox_lifecycle",
     # "netbox_otp_plugin", # Outdated as of 20260726
     "netbox_prometheus_sd",
+    "netbox_qrcode",
+    "netbox_reorder_rack",
     "netbox_routing",
+    "netbox_secrets",
     "netbox_security",
+    "netbox_topology_views",
     "validity",
-    "netbox_branching",
+    # "slurpit_netbox",
 ]
 
 PLUGINS_CONFIG = {
